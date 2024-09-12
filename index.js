63fdcf2bd43cc061ee7a57b70ae52609f78bc402
CodeSandbox <support@codesandbox.io>
2024-09-12 11:18:47 +0000

    Sandbox snapshot commit

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("Hello CodeSandbox");
+var letters = ["a", "b", "c", "d", "e", "f"];
+
+var generateYesPerLetter = function () {
+  var myOutputValue = "";
+  var counter = 0;
+
+  while (counter < letters.length) {
+    myOutputValue = myOutputValue + "yes";
+    counter = counter + 1;
+  }
+  return myOutputValue;
+};
+
+console.log(generateYesPerLetter());
+
+// QUESTION 5: This is a sample code snippet of a phone book. Explain what this code does.
+var names = [];
+var findName = function (input) {
+  var found = false;
+  var index = 0;
+  while (index < names.length) {
+    var currentName = names[index];
+    if (currentName == input) {
+      found = true;
+    }
+    index = index + 1;
+  }
+
+  console.log(found);
+  if (!found) {
+    names.push(input);
+  }
+
+  var myOutputValue = "All names: " + names;
+  return myOutputValue;
+};
